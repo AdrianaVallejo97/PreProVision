87e8399c46926c4c2bbff7fa72a2cf45ac90e94d
Adriana Vallejo <atvallejomunoz@hotmail.com>
2026-01-18 18:18:33 -0500

    feat(user): set up server entry point

---
+++ Backend/user-service/server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();   // 👈 FALTA ESTO
+
+const app = require("./app");
+
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, () => {
+  console.log(`user-service running on port ${PORT}`);
+});
