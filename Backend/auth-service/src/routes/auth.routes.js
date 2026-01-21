88e7c9b14cfe4963cd3ebf0a669637808238117b
Adriana Vallejo <atvallejomunoz@hotmail.com>
2026-01-20 23:21:44 -0500

    feat(auth-service): actualiza rutas de autenticación

--- Backend/auth-service/src/routes/auth.routes.js
+++ Backend/auth-service/src/routes/auth.routes.js
@@ -1,29 +1,19 @@
 const express = require("express");
 const { body } = require("express-validator");
-const { login, me } = require("../controllers/auth.controller.js");
-const { requireAuth } = require("../middlewares/auth.middleware.js");
-const { requireRole } = require("../middlewares/role.middleware.js");
-
-
+const { login, me } = require("../controllers/auth.controller");
+const { requireAuth } = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
-// POST /auth/login
 router.post(
   "/login",
   [
-    body("email").isEmail().withMessage("email must be valid"),
-    body("password").isString().isLength({ min: 6 }).withMessage("password min 6")
+    body("email").isEmail(),
+    body("password").isString().isLength({ min: 6 })
   ],
   login
 );
 
-// GET /auth/me
 router.get("/me", requireAuth, me);
-
-// Ejemplo: endpoint protegido solo ADMIN
-router.get("/admin-only", requireAuth, requireRole("ADMIN"), (req, res) => {
-  res.json({ ok: true, message: "Hello Admin" });
-});
 
 module.exports = { authRouter: router };
