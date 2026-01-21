c7a7e6927d7adf8def44a9fd839568b5cd277958
Adriana Vallejo <atvallejomunoz@hotmail.com>
2026-01-21 00:12:45 -0500

    feat(api-gateway): add app configuration

---
+++ Backend/api-Gateway/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const { gatewayRouter } = require("./routes/gateway.routes");
+const { errorMiddleware } = require("./middlewares/error.middleware");
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(morgan("combined"));
+
+app.use("/", gatewayRouter);
+
+app.use(errorMiddleware);
+
+module.exports = app;
