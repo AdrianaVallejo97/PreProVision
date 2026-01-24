129a2f31de67f75bad6c5203503b56a6b9924d73
Adriana Vallejo <atvallejomunoz@hotmail.com>
2026-01-23 23:21:06 -0500

    feat(monitoring-service): add HTTP server

--- Backend/monitoring-service/server.js
+++ Backend/monitoring-service/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = Number(process.env.PORT) || 3007;
+const PORT = Number(process.env.PORT) || 3010;
 
 app.listen(PORT, () => {
   console.log(`monitoring-service running on port ${PORT}`);
